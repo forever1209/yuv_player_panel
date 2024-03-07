43fe98aa258a898730adf2be3b09efce5b4c5014
liulei10 <liulei10@megvii.com>
2024-03-07 16:05:19 +0800

    feat(ffmpeg):add ffmpeg tool

--- include/ffmpeg/ffmpeg_tool.h
+++ include/ffmpeg/ffmpeg_tool.h
@@ -2,6 +2,7 @@ extern "C" {
 #include "libavutil/imgutils.h"
 #include "libswscale/swscale.h"
 #include "libavutil/frame.h"
+#include "libavcodec/avcodec.h"
 }
 
 namespace mc
@@ -17,6 +18,11 @@ namespace mc
             void InitDecoder();
             static void ConvertYUYVtoYUV(unsigned char* yuyvData, AVFrame  *frmyuyv,int width, int height) ;
             AVFrame* DecodeOneFrame(const uint8_t* data,const  int &size);
+        private:
+            AVCodec* codec;
+            AVCodecContext* codecContext;
+            AVFrame* frame;
+            bool m_bInit = false;
         };
     } // namespace video
     
--- src/ffmpeg_tool.cpp
+++ src/ffmpeg_tool.cpp
@@ -1,17 +1,28 @@
 #include "ffmpeg/ffmpeg_tool.h"
+#include <iostream>
 namespace mc
 {
     namespace video
     {
         FFmpegTool::FFmpegTool()
         {
-            
+            m_bInit = false;
         }
         FFmpegTool::~FFmpegTool()
         {
+            if(m_bInit)
+            {
+                avcodec_free_context(&codecContext);
+                av_frame_free(&frame);
+            }           
         }
         void FFmpegTool::InitDecoder()
         {
+            codec = avcodec_find_decoder(AV_CODEC_ID_H264);
+            codecContext = avcodec_alloc_context3(codec);
+            avcodec_open2(codecContext, codec, nullptr);
+            frame = av_frame_alloc();
+            m_bInit= true;
         }
         void FFmpegTool::ConvertYUYVtoYUV(unsigned char *yuyvData, AVFrame *frm420p, int width, int height)
         {
@@ -45,7 +56,33 @@ namespace mc
         }
         AVFrame *FFmpegTool::DecodeOneFrame(const uint8_t *data, const int &size)
         {
-            return nullptr;
+            AVPacket *packet = av_packet_alloc();
+            packet->data = const_cast<uint8_t*>(data);
+            packet->size = size;
+
+            int response = avcodec_send_packet(codecContext, packet);
+            if (response < 0) {
+                std::cerr << "无法发送视频包给解码器" << std::endl;
+                av_packet_free(&packet); // 释放 AVPacket 内存
+                return nullptr;
+            }
+
+            response = avcodec_receive_frame(codecContext, frame);
+            if (response == AVERROR(EAGAIN) || response == AVERROR_EOF) {
+                av_packet_free(&packet); // 释放 AVPacket 内存
+                return nullptr;
+            }
+            else if (response < 0) {
+                std::cerr << "无法从解码器接收视频帧" << std::endl;
+                av_packet_free(&packet); // 释放 AVPacket 内存
+                return nullptr;
+            }
+
+            // 处理视频帧数据，这里简单打印宽高信息
+            std::cout << "解码得到视频帧，宽度：" << frame->width << "，高度：" << frame->height << std::endl;
+
+            av_packet_free(&packet); // 释放 AVPacket 内存
+            return frame;
         }
     } // namespace video
 
